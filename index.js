f9282b58d242b04af5c706c947a688551efa1455
Duong Van Thien <duongvanthienbkhoa@gmail.com>
2020-03-29 23:12:36 +0700

    Khoi tao database

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// Khoi tao database
+const db = require('./db');
+db.defaults({ users: [] }).write();
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Server da duoc khoi tao tai cong ${port}!`));
