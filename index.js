c59e33495e9ba974d87307b50c85449bda3350dc
Duong Van Thien <duongvanthienbkhoa@gmail.com>
2020-03-29 23:32:46 +0700

    setup template engine va tao template layout

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 5000;
 const db = require('./db');
 db.defaults({ users: [] }).write();
 
+// Khoi tao template engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Server da duoc khoi tao tai cong ${port}!`));
